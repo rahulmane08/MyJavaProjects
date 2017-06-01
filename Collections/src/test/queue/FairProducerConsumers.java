cfb89c104c72b8644de556d10271a24f946c0659
rahulmane08 <rahul.mane08@gmail.com>
2017-06-01 13:23:00 +0530

    Create FairProducerConsumers.java

---
+++ Collections/src/test/queue/FairProducerConsumers.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
+public class FairProducerConsumers {
+	public static void main(String[] args) {
+		BlockingQueue<String> bq = new ArrayBlockingQueue<>(10,false);
+		List<String> elements = new ArrayList<>();
+		for(int i=0;i<10;i++)
+			elements.add("elem"+i);
+		Thread producer = new Thread(new Producer(bq,elements));
+	    Thread consumer1 = new Thread(new Consumer(bq,elements,"Consumer1"));
+	    Thread consumer2 = new Thread(new Consumer(bq,elements,"Consumer2"));
+	    producer.start();
+	    consumer1.start();
+	    consumer2.start();
+	}
+}
