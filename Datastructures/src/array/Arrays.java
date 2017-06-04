21740721c754b95fe2e4c794a9ad9d07819215ae
rahul <rahul.mane08@gmail.com>
2017-06-04 16:56:47 +0530

    commit

--- Datastructures/src/array/Arrays.java
+++ Datastructures/src/array/Arrays.java
@@ -58,179 +58,79 @@ public class Arrays {
 		System.out.println(java.util.Arrays.toString(xor));
 	}
 
-	static public void toSubArraysWithEqualSum(int[] arr)
-
-	{
-
+	static public void toSubArraysWithEqualSum(int[] arr) {
 		int end = 0;
-
 		int sum = 0;
-
 		int n = arr.length;
-
 		for (int i : arr)
-
 			sum += i;
-
 		int halfSum = sum / 2;
-
-		for (int i = 0; i < n; i++)
-
-		{
-
+		for (int i = 0; i < n; i++) {
 			sum = sum - arr[i];
-
-			if (sum == halfSum)
-
-			{
-
+			if (sum == halfSum) {
 				end = i;
-
 				break;
-
-			}
-
-			else if (sum < arr[i])
-
-			{
-
+			} else if (sum < arr[i]) {
 				System.out.println("no subarrays");
-
 				return;
-
 			}
-
 		}
-
 		System.out.println(
 				"first subarray = " + java.util.Arrays.toString(java.util.Arrays.copyOfRange(arr, 0, end + 1)));
-
 		System.out.println(
 				"second subarray = " + java.util.Arrays.toString(java.util.Arrays.copyOfRange(arr, end + 1, n)));
-
 	}
 
-	static public int[][] createSnakeAndLadderPattern(int n)
-
-	{
-
+	static public int[][] createSnakeAndLadderPattern(int n) {
 		int[][] board = new int[n][n];
-
 		boolean odd = false;
-
-		for (int i = 0; i < n; i++)
-
-		{
-
-			for (int j = 0; j < n; j++)
-
-			{
-
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
 				int elem = 5 * i + j + 1;
-
 				if (!odd)
-
 					board[i][j] = elem;
-
 				else
-
 					board[i][n - j - 1] = elem;
-
 			}
-
 			odd = !odd;
-
 		}
-
 		return board;
-
 	}
 
-	static public int[] findMinUnsortedSubArrayToSortEntireArray(int[] arr)
-
-	{
-
+	static public int[] findMinUnsortedSubArrayToSortEntireArray(int[] arr) {
 		int n = arr.length;
-
 		int start = 0;
-
 		int end = n - 1;
-
 		for (int i = 0; i < n; i++)
-
-			if (arr[i] > arr[i + 1])
-
-			{
-
+			if (arr[i] > arr[i + 1]) {
 				start = i;
-
 				break;
-
 			}
-
 		for (int i = n - 1; i >= 0; i--)
-
-			if (arr[i] < arr[i - 1])
-
-			{
-
+			if (arr[i] < arr[i - 1]) {
 				end = i;
-
 				break;
-
 			}
-
 		int min, max;
-
 		min = max = arr[start];
-
-		for (int i = start; i <= end; i++)
-
-		{
-
+		for (int i = start; i <= end; i++) {
 			if (arr[i] < min)
-
 				min = arr[i];
-
 			if (max < arr[i])
-
 				max = arr[i];
-
 		}
-
-		for (int i = 0; i < start; i++)
-
-		{
-
-			if (arr[i] > min)
-
-			{
-
+		for (int i = 0; i < start; i++) {
+			if (arr[i] > min) {
 				start = i;
-
 				break;
-
 			}
-
 		}
-
-		for (int i = n - 1; i > end; i--)
-
-		{
-
-			if (arr[i] < max)
-
-			{
-
+		for (int i = n - 1; i > end; i--) {
+			if (arr[i] < max) {
 				end = i;
-
 				break;
-
 			}
-
 		}
-
 		return java.util.Arrays.copyOfRange(arr, start, end + 1);
-
 	}
 }
