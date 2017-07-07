54413acb8a9620605f355e57f653e4dbc8a50bc3
rahulmane08 <rahul.mane08@gmail.com>
2017-07-07 11:09:12 +0530

    Update Graph.java

--- Datastructures/src/graph/Graph.java
+++ Datastructures/src/graph/Graph.java
@@ -12,12 +12,14 @@ public class Graph<T>
 	
 	private final Map<ListNode<T>, LinkedList<T>> map;
 	private final int count;
-	public Graph(T[] vertices)
+	private final boolean directed;
+	public Graph(T[] vertices, boolean directed)
 	{
 		count=vertices.length;
 		map = new HashMap<>();
 		for(int i=0;i<count;i++)
 			map.put(new ListNode<>(vertices[i]), new LinkedList<>());
+		this.directed=directed;
 	}
 	
 	public void addEdge(T source , T dest)
@@ -27,8 +29,12 @@ public class Graph<T>
 		list.insert(dest);
 		
 		//also add edge from dest to source
-		list = map.get(new ListNode<T>(dest));
-		list.insert(source);
+		if(!directed)
+		{
+			list = map.get(new ListNode<T>(dest));
+			list.insert(source);
+		}
+		
 	}
 	
 	public LinkedList<T> getConnectedNodes(ListNode<T> data)
@@ -42,5 +48,16 @@ public class Graph<T>
 		for(Entry<ListNode<T>, LinkedList<T>> entry: map.entrySet())
 			s += "Vertex:"+entry.getKey()+", connects to "+entry.getValue()+"\n";
 		return s;
+	}
+	
+	public ListNode<T>[] getVertices()
+	{
+		ListNode<T>[] arr = new ListNode[map.size()];
+		int index = 0;
+		for(ListNode<T> vertex: map.keySet())
+		{
+			arr[index++] = vertex;
+		}
+		return arr;
 	}
 }
