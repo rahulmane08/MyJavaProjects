bcc57aa043d005ace008b7b19a84deabf87c5752
rahulmane08 <rahul.mane08@gmail.com>
2017-07-07 16:24:54 +0530

    Create GraphTraversal.java

---
+++ Datastructures/src/graph/traversal/GraphTraversal.java
@@ -0,0 +1,64 @@
+package graph.traversal;
+
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
+
+import graph.Graph;
+import graph.Vertex;
+
+public class GraphTraversal 
+{
+	static public <T> void bfs(Graph<T> graph)
+	{
+		if(graph==null)
+			return;
+		System.out.println("BFS Traversal============");
+		Queue<Vertex<T>> queue = new LinkedList<>();
+		HashSet<Long> visited = new HashSet<>();
+		
+		for(Vertex<T> vertex: graph.getAllVertexes())
+		{
+			if(!visited.contains(vertex.getId()))
+			{
+				queue.offer(vertex);
+				visited.add(vertex.getId());
+				
+				while(!queue.isEmpty())
+				{
+					Vertex<T> curr = queue.poll();
+					System.out.println(curr.getData());
+					for(Vertex<T> adjacentVertex: curr.getAdjacentVertexes())
+					{
+						if(!visited.contains(adjacentVertex.getId()))
+						{
+							queue.offer(adjacentVertex);
+							visited.add(adjacentVertex.getId());
+						}
+					}
+				}
+			}			
+		}
+	}
+	
+	static public <T> void dfs(Graph<T> graph)
+	{
+		if(graph==null)
+			return;
+		System.out.println("DFS Traversal============");
+		HashSet<Long> visited = new HashSet<>();
+		for(Vertex<T> vertex: graph.getAllVertexes())
+			if(!visited.contains(vertex.getId()))
+				dfsUtil(vertex, visited);
+				
+	}
+	
+	static private <T> void dfsUtil(Vertex<T> vertex,HashSet<Long> visited)
+	{
+		visited.add(vertex.getId());
+		System.out.println(vertex.getData());
+		for(Vertex<T> adjacentVertex: vertex.getAdjacentVertexes())
+			if(!visited.contains(adjacentVertex.getId()))
+				dfsUtil(adjacentVertex, visited);
+	}
+}
