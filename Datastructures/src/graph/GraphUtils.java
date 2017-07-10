f12f84b484928b3ea61a7e2af3b825d610b4cd73
rahulmane08 <rahul.mane08@gmail.com>
2017-07-10 16:05:38 +0530

    Update GraphUtils.java

--- Datastructures/src/graph/GraphUtils.java
+++ Datastructures/src/graph/GraphUtils.java
@@ -13,4 +13,22 @@ public class GraphUtils
 			graph.addEdge(edge.getVertex2().getId(), edge.getVertex1().getId(), edge.getWeight());
 		return transpose;				
 	}	
+	static private <T> boolean areConnected(Vertex<T> v1, Vertex<T> v2, Graph<T> graph,HashSet<Long> visited)
+	{
+		if(graph!=null)
+		{			
+			for(Vertex<T> v: v1.getAdjacentVertexes())
+				if(!visited.contains(v.getId()))
+				{
+					visited.add(v.getId());
+					if(v.equals(v2))
+						return true;
+					else
+						return areConnected(v, v2, graph,visited);
+				}
+					
+		}
+		
+		return false;		
+	}
 }
