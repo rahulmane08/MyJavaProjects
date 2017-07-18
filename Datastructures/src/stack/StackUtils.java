e1023cf2093287b6a6ba466f49e2fef522e301c5
rahulmane08 <rahul.mane08@gmail.com>
2017-07-18 18:57:16 +0530

    Update StackUtils.java

--- Datastructures/src/stack/StackUtils.java
+++ Datastructures/src/stack/StackUtils.java
@@ -168,25 +168,30 @@ public class StackUtils {
 		s.push(curr);
 	}
 
-	public static int[] findSpan(int[] arr) {
+	/**
+	 * The span Si of the stock’s price on a given day i is defined as the maximum number of consecutive days just before the given day, for which the price of the stock on the current day is less than or equal to its price on the given day.
+		For example, if an array of 7 days prices is given as {100, 80, 60, 70, 60, 75, 85}, then the span values for corresponding 7 days are {1, 1, 1, 2, 1, 4, 6}
+	 * @param arr
+	 * @return
+	 */
+	static public int[] findSpan(int [] arr)
+	{
+		Stack<Integer> s = new Stack<>();
 		int[] span = new int[arr.length];
 		span[0] = 1;
-		stack.Stack<Integer> stack = new stack.Stack<>(arr.length);
-		for (int i = 1; i < arr.length; i++) {
-			int p = 0;
-			while (!stack.isEmpty())
-				if (arr[i] > arr[stack.top()])
-					stack.pop();
-				else
-					break;
-			if (stack.isEmpty())
-				p = -1;
+		s.push(0);
+		for(int i=1;i<arr.length;i++)
+		{
+			
+			while(!s.isEmpty() && arr[i]>arr[s.peek()])
+				s.pop();
+			
+			if(s.isEmpty())
+				span[i] = i+1;
 			else
-				p = stack.top();
-			span[i] = i - p;
-			stack.push(i);
-		}
-
+				span[i] = i-s.peek();
+			s.push(i);
+		}		
 		return span;
 	}
 
