447959babd84325105e0d753a5550566ddb7bd11
rahulmane08 <rahul.mane08@gmail.com>
2017-07-24 18:49:54 +0530

    Update ProducerConsumerUsingBQ.java

--- Collections/src/test/queue/ProducerConsumerUsingBQ.java
+++ Collections/src/test/queue/ProducerConsumerUsingBQ.java
@@ -1,18 +1,23 @@
-import java.util.ArrayList;
-import java.util.List;
+package test.queue;
+
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 
+
+
 public class ProducerConsumerUsingBQ {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		BlockingQueue<String> bq = new ArrayBlockingQueue<>(10);
-		List<String> elements = new ArrayList<>();
-		for(int i=0;i<10;i++)
-			elements.add("elem"+i);
-		Thread producer = new Thread(new Producer(bq,elements));
-	    Thread consumer = new Thread(new Consumer(bq,elements, "Consumer"));
+		Thread producer = new Thread(new Producer(bq, "Producer"));
+	    Thread consumer = new Thread(new Consumer(bq, "Consumer"));
 	    producer.start();
 	    consumer.start();
+	    
+	    Thread.sleep(60*1000);
+	    producer.interrupt();
+	    consumer.interrupt();
+	    producer.join();
+	    consumer.join();
 	}
 }
