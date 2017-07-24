33f293e6a01d118dea262c1bdcc4d23600a61b2e
rahulmane08 <rahul.mane08@gmail.com>
2017-07-24 18:39:55 +0530

    Update Producer.java

--- Collections/src/test/queue/Producer.java
+++ Collections/src/test/queue/Producer.java
@@ -1,29 +1,33 @@
 package test.queue;
 
-import java.util.List;
 import java.util.concurrent.BlockingQueue;
 
 class Producer<T> implements Runnable {
-	BlockingQueue<T> queue;
-	List<T> elements;
-	public Producer(BlockingQueue<T> queue, List<T> elements) 
+	BlockingQueue<T> queue;	
+	String name;
+	int elemNumber=0;
+	public Producer(BlockingQueue<T> queue, String name) 
 	{
 		super();
-		this.queue = queue;
-		this.elements = elements;
+		this.queue = queue;		
+		this.name = name;
 	}
 
 	@Override
 	 public void run() {
 	      System.out.println("Producer Started");
-	      try {	    	 
-	         while(!elements.isEmpty())
-	         {	
-	        	 Thread.sleep(1000);
-	        	 queue.put(elements.remove(0));
-	         }
-	      } catch (Exception ex) {
-	         ex.printStackTrace();
+	      while(!Thread.currentThread().isInterrupted())
+	      {
+		      try {
+		        	 Thread.sleep(1000);
+		        	T elem = (T) ("elem"+(elemNumber++)); 
+		        	 System.out.println(name+" putting an element = "+elem);
+		        	 queue.put(elem);
+		        
+		      } catch (Exception ex) {
+		    	  System.out.println(name+" is interrupted");
+				  Thread.currentThread().interrupt();
+		      }
 	      }
 	 }
 	
