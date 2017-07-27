0a554dd61f1e545bfb3a8b24bdd5abbff86f7563
rahulmane08 <rahul.mane08@gmail.com>
2017-07-27 16:21:12 +0530

    Update AVLTree.java

--- Datastructures/src/tree/AVLTree.java
+++ Datastructures/src/tree/AVLTree.java
@@ -1,48 +1,83 @@
 package tree;
 
-public class AVLTree
+public class AVLTree extends BinarySearchTree 
 {
-	public Node root;
-
-	public AVLTree() {
-		super();
+	private Node rotateRight(Node root)
+	{
+		Node newRoot = root.left;
+		root.left = newRoot.right;
+		newRoot.right = root;
+		root.height = TreeUtils.heightRecursive(root);
+		newRoot.height = TreeUtils.heightRecursive(newRoot);
+		return newRoot;
 	}
 	
-	public int height(Node node)
+	private Node rotateLeft(Node root)
 	{
-		if(node==null)
-			return 0;
-		return 1+Math.max(height(node.left),height(node.right));
+		Node newRoot = root.right;
+		root.left = newRoot.left;
+		newRoot.left = root;
+		root.height = TreeUtils.heightRecursive(root);
+		newRoot.height = TreeUtils.heightRecursive(newRoot);
+		return newRoot;
 	}
 	
-	public int balance(Node node)
+	private int getBalance(Node root)
 	{
-		return height(node.left) - height(node.right);
-	}
-	
-	
-	
-	public Node insert(Node node, int data)
-	{
-		/** 1. regular binary tree insert**/
 		if(root==null)
+			return 0;
+		return root.left.height - root.right.height; 
+	}	
+	
+	public Node insert(Node node, int data) 
+	{		
+		Node newNode = new Node(null, null, data);
+ 		if(root==null)
 		{
-			root = new Node(data);
-			return root;
-		}
+ 			root = newNode;
+ 			System.out.println("root node created"); 			
+		}	
+		
+		if(node.data==data)
+		{
+			System.out.println(data+" already present");			
+		}			
 		if(data<node.data)
-			node.left = insert(node.left,data);
-		else if(data>node.data)
-			node.right = insert(node.right,data);
-		else
-			return node;
+		{
+			if(node.left==null)
+				node.left=newNode;
+			else
+				return insert(node.left,data);
+		}		
+		if(data>node.data)
+		{
+			if(node.right==null)
+				node.right=newNode;
+			else
+				 return insert(node.right,data);
+		}
 		
-		/** 2. update the height and compute the balance**/
-		node.height = height(node);
-		int balance = this.balance(node);
+		//update the height of ancestor
+		node.height = TreeUtils.heightRecursive(node);
 		
-		/** 3.  **/
+		int balance = getBalance(node);
+		if(balance>1 && data<node.left.data) //LEFT - LEFT
+			return rotateRight(node);
 		
+		if(balance>1 && data>node.left.data) //LEFT - RIGHT
+		{
+			node.left = rotateLeft(node.left);
+			return rotateRight(node);
+		}
+			
+		if(balance>1 && data<node.right.data) //RIGHT - RIGHT
+			return rotateLeft(node);
+		
+		if(balance>1 && data>node.right.data) //RIGHT - LEFT
+		{
+			node.right = rotateRight(node.right);
+			return rotateLeft(node);
+		}
 		
 		return node;
 	}
