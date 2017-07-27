44a3ee0c7ba9867894c552a4064b85c6d8e6d3b4
rahulmane08 <rahul.mane08@gmail.com>
2017-07-27 17:14:06 +0530

    Update FindLongestPath.java

--- Algorithms/src/backtracking/FindLongestPath.java
+++ Algorithms/src/backtracking/FindLongestPath.java
@@ -1,13 +1,66 @@
 package backtracking;
 
+import java.util.LinkedHashSet;
+import java.util.Set;
+
 public class FindLongestPath {
 	private final int[][] arr, visited;
 	private final int M;
 	private final int N;
 	private final int[] xOptions = {-1,0, 0,1};
-	private final int[] yOptions = { 0,1,-1,0};
-	private int count = Integer.MIN_VALUE;
+	private final int[] yOptions = { 0,1,-1,0};	
+	private Set<Point> maxPath = new LinkedHashSet<>();
 	
+	class Point
+	{
+		int x,y;
+		
+		
+
+		@Override
+		public String toString() {
+			return "Point [x=" + x + ", y=" + y + "]";
+		}
+
+		public Point(int x, int y) {
+			super();
+			this.x = x;
+			this.y = y;
+		}
+
+		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			result = prime * result + getOuterType().hashCode();
+			result = prime * result + x;
+			result = prime * result + y;
+			return result;
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			Point other = (Point) obj;
+			if (!getOuterType().equals(other.getOuterType()))
+				return false;
+			if (x != other.x)
+				return false;
+			if (y != other.y)
+				return false;
+			return true;
+		}
+
+		private FindLongestPath getOuterType() {
+			return FindLongestPath.this;
+		}
+		
+	}
 	public FindLongestPath(int[][] arr, int m, int n) {
 		super();
 		this.arr = arr;
@@ -30,19 +83,27 @@ public class FindLongestPath {
 	
 	public void solve(int startX,int startY, int endX, int endY)
 	{
-		trace(startX, startY, endX, endY, 0);
-		if(count==Integer.MIN_VALUE)
+		trace(startX, startY, endX, endY, 0, new LinkedHashSet<>());
+		if(maxPath.size() == 0)
 			System.out.println("No Path found from ("+startX+","+startY+") and ("+endX+","+endY+")");
 		else
-			System.out.println("Path found from ("+startX+","+startY+") and ("+endX+","+endY+") of max length = "+count);
+		{
+			System.out.println("Path found from ("+startX+","+startY+") and ("+endX+","+endY+") of max length = "+maxPath.size());
+			maxPath.forEach(e->{System.out.println(e);});
+		}
+			
 	}
 	
-	private void trace(int startX,int startY, int endX, int endY, int currCount)
+	private void trace(int startX,int startY, int endX, int endY, int currCount, LinkedHashSet<Point> currPath)
 	{
 		visited[startX][startY]=1;
+		Point p = new Point(startX, startY);
+		currPath.add(p);
 		if(startX==endX && startY == endY)
 		{
-			count = Math.max(count, currCount);
+			if(currPath.size()>maxPath.size())
+				maxPath = new LinkedHashSet<>(currPath);			
+			
 		}		
 		for(int i=0;i<4;i++)
 		{
@@ -50,9 +111,10 @@ public class FindLongestPath {
 			int y = yOptions[i]+startY;
 			if(isSafe(x, y))
 			{				
-				trace(x, y, endX, endY, ++currCount);				
+				trace(x, y, endX, endY, ++currCount,currPath);				
 			}
-		}
+		}		
+		currPath.remove(p);
 		visited[startX][startY]=-1;
 	}
 	
@@ -60,8 +122,8 @@ public class FindLongestPath {
 		int[][] arr = {
 						{1,1,1,1,1},
 						{1,0,1,0,1},
-						{1,0,1,0,0},
-						{1,0,1,0,0},
+						{1,1,1,0,1},
+						{1,1,1,0,1},
 						{1,1,1,1,1}
 					  };
 		new FindLongestPath(arr, 5, 5).solve(1, 4, 4, 4);
