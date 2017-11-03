3f9739a7db4b89c7a6af530b923b76ca055b8640
rahulmane08 <rahul.mane08@gmail.com>
2017-11-03 13:16:14 +0530

    working employee service

---
+++ MyXenonProject/src/main/java/com/rmane/xenon/practice/DemoHost.java
@@ -0,0 +1,32 @@
+package com.rmane.xenon.practice;
+
+import com.vmware.xenon.common.ServiceHost;
+import com.vmware.xenon.services.common.RootNamespaceService;
+
+import java.nio.file.Paths;
+
+public class DemoHost extends ServiceHost
+{
+    public static void main(String[] args) throws Throwable{
+        ServiceHost.Arguments defaultArgs = new ServiceHost.Arguments();
+        defaultArgs.sandbox = Paths.get("/Users/rmane/xenon");
+        DemoHost host = new DemoHost();
+        Runtime.getRuntime().addShutdownHook(new Thread(()->host.stop()));
+        host.initialize(args, defaultArgs);
+        host.start();
+    }
+
+    @Override
+    public ServiceHost start() throws Throwable {
+        super.start();
+        startDefaultCoreServicesSynchronously();
+
+        //start enumeration service
+        this.startService(new RootNamespaceService());
+
+        //start the employee factory service
+        this.startService(new EmployeeFactoryService());
+
+        return this;
+    }
+}
